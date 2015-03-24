21fc043d02d08b6f040390f15bfddb0e30f07045
Paul Harter <paul@glowinthedark.co.uk>
2015-03-24 12:54:04 +0000

    catch impost failure for couchbase server

--- src/fam/database/__init__.py
+++ src/fam/database/__init__.py
@@ -1,3 +1,7 @@
-from .couchbase_server import CouchbaseWrapper
+try:
+    from .couchbase_server import CouchbaseWrapper
+except ImportError:
+    print "Couldnt import CouchbaseWrapper"
+
 from .couchdb import CouchDBWrapper
 from .sync_gateway import SyncGatewayWrapper
