d95d0e69f684a95f83039d86792c4f374c4df6a0
Paul Harter <paul@glowinthedark.co.uk>
2016-01-08 18:57:35 +0000

    trying to build latest sync gateway

---
+++ build_test_gateway.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+mkdir -p /opt
+apt-get update
+apt-get -q -y install git bc wget build-essential
+wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
+
+tar -C /usr/local -xzf go1.4.2.linux-amd64.tar.gz
+
+export PATH=$PATH:/usr/local/go/bin
+
+cd /opt
+
+git clone https://github.com/couchbase/sync_gateway.git
+
+cd /opt/sync_gateway
+git submodule init
+git submodule update
+./build.sh
+cp bin/sync_gateway /usr/local/bin
+
+mkdir -p /opt/sync_gateway/data
+rm go1.4.2.linux-amd64.tar.gz
+rm -rf /opt/sync_gateway
