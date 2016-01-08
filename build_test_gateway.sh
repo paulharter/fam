c64187c95291057eab0855435dd4e3ad4ba42f2c
Paul Harter <paul@glowinthedark.co.uk>
2016-01-08 19:43:03 +0000

    fed up with circle

--- build_test_gateway.sh
+++ build_test_gateway.sh
@@ -1,19 +1,8 @@
 #! /bin/sh
 
-#mkdir -p /opt
-#apt-get update
-#apt-get -q -y install git bc wget build-essential
-#wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
-#
-#tar -C /usr/local -xzf go1.4.2.linux-amd64.tar.gz
-#
-#export PATH=$PATH:/usr/local/go/bin
-
-
-sudo git clone git@github.com:paulharter/sync_gateway.git
-sudo cd sync_gateway
-sudo git submodule init
-sudo git submodule update
-sudo ./build.sh
-sudo cp bin/sync_gateway /usr/local/bin
+cd sync_gateway
+git submodule init
+git submodule update
+./build.sh
+cp bin/sync_gateway /usr/local/bin
 
