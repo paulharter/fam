2784add82a76ea229b0039d4b14caaa68f4e87c7
Paul Harter <paul@glowinthedark.co.uk>
2018-02-22 14:57:19 +0000

    managed to put slimit fix post dep install

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+
 import atexit
 from setuptools import setup, find_packages
 from setuptools.command.develop import develop
@@ -11,34 +12,42 @@ def delete_old_slimit_files():
     slimit install ships with out of date versions of these files that have been created by ply 3.4
     To use ply 3.8 with slimit 0.8.1 you have to delete thses two files, they will be lazily recreated by slimit using the up to date ply
     """
+    import site
+    from importlib import reload
+    reload(site)
+    import sys
+    def find_module_path():
+        for p in sys.path:
+            if os.path.isdir(p) and "slimit" in os.listdir(p):
+                return os.path.join(p, "slimit")
 
-    import slimit
+    install_path = find_module_path()
+
     file_names = ["lextab.py", "yacctab.py"]
-    dir = os.path.dirname(slimit.__file__)
     for file_name in file_names:
-        file_path = os.path.join(dir, file_name)
+        file_path = os.path.join(install_path, file_name)
         if os.path.exists(file_path):
             os.remove(file_path)
 
 
 class PostDevelopCommand(develop):
     """Post-installation for development mode."""
-    def run(self):
-        develop.run(self)
+
+    def __init__(self, *args, **kwargs):
+        super(PostDevelopCommand, self).__init__(*args, **kwargs)
         def _post_install():
             delete_old_slimit_files()
         atexit.register(_post_install)
-
 
 
 class PostInstallCommand(install):
     """Post-installation for installation mode."""
-    def run(self):
-        install.run(self)
+
+    def __init__(self, *args, **kwargs):
+        super(PostInstallCommand, self).__init__(*args, **kwargs)
         def _post_install():
             delete_old_slimit_files()
         atexit.register(_post_install)
-
 
 
 setup(name='fam',
@@ -55,7 +64,7 @@ setup(name='fam',
     author='Paul Harter',
     author_email='paul@glowinthedark.co.uk',
     license="LICENSE",
-    install_requires=['js2py', 'requests', 'simplejson', 'jsonschema', 'mock', 'pytz', 'slimit', 'ply',
+    install_requires=['js2py', 'requests', 'simplejson', 'jsonschema', 'mock', 'pytz', 'slimit', 'ply==3.8',
                     'firebase_admin'],
     packages=find_packages('src'),
     package_dir={'': 'src'},
