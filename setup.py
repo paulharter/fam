7c61d02f62ead468b8ae9fd6122d869c6599a1b7
Paul Harter <paul@glowinthedark.co.uk>
2018-12-20 16:06:06 +0000

    patch version bump

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ class PostInstallCommand(install, object):
 
 
 setup(name='fam',
-    version='2.0.9',
+    version='2.0.10',
     description="Simple Python ORM for CouchDB, and Sync Gateway",
     url="https://github.com/paulharter/fam",
     classifiers=[
